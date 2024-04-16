3124bfc1d2f40fb129d0273146c09f800fc39732
hhg.mzt <hiram.hdezg@gmail.com>
2024-04-16 12:36:34 -0700

    chore: se renderiza el total de productos en el template totales.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from sales.service import SaleService
 from categories.category_service import CategoryService
 from clients.service import  ClienteService
+from products.service import ProductService
 # Create your views here.
 
 def show_dashboard(request):
@@ -11,12 +12,14 @@ def show_dashboard(request):
 
     category_serivice = CategoryService()
     cliente_service = ClienteService()
+    product_service = ProductService()
 
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     context={
         'total_ventas': sale_total,
         'total_categorias': category_serivice.total_categories(),
-        'total_clientes': cliente_service.total_clientes()
+        'total_clientes': cliente_service.total_clientes(),
+        'total_productos': product_service.get_total()
     }
 
     return render(request, 'dashboard.html', context=context)
