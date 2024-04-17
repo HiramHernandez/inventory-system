9a0abb1ca2f68828ee151c58dd07ecdf9fb9fc62
hhg.mzt <hiram.hdezg@gmail.com>
2024-04-17 11:02:07 -0700

    chore: se desacopla la función de renderizar la gráfica hacia un script propio

---
+++ static/js/grafica_ventas.js
@@ -0,0 +1,37 @@
+let ventasPorDiaUrl = '/dashboard/ventas-por-dia';
+$(document).ready(function () {
+    // Solicitar datos de la API usando AJAX
+    $.getJSON(ventasPorDiaUrl, function (data) {
+        // Procesar los datos obtenidos
+        var datosGrafico = []; // Arreglo para almacenar los puntos del gráfico
+        for (var i = 0; i < data.length; i++) {
+            var punto = {
+                fecha__date: data[i].fecha__date,
+                ventas_por_dia: data[i].ventas_por_dia,
+                total_por_dia: data[i].total_por_dia
+            };
+            datosGrafico.push(punto);
+        }
+
+        // Crear el gráfico de líneas de Morris usando los datos procesados
+        new Morris.Line({
+            element: 'line-chart-ventas',
+            data: datosGrafico,
+            xkey: 'fecha__date',
+            ykeys: ['total_por_dia'],
+            labels: ['Valor'],
+            axes: true, // Habilitar ejes (necesario para la configuración)
+            grid: true, // Habilitar cuadrícula
+            yaxes: [
+                {
+                    grid: true, // Habilitar cuadrícula en el eje Y
+                    beginAtZero: false, // Permitir que el eje Y comience en un valor distinto de cero
+                    // Opcional: Especificar el rango máximo del eje Y (en este caso, lo configuraremos dinámicamente)
+                    max: function (data) {
+                        return Math.max(...data.map(function (row) { return row.y; }));
+                    }
+                }
+            ]
+        });
+    });
+});
